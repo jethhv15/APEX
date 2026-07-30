6a353cfe25d1b8a577b236b8d0bc432811dd2b72
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 12:17:52 +0700

    refactor(lib): improve file helper library

--- lib/file.sh
+++ lib/file.sh
@@ -3,14 +3,10 @@
 # APEX
 # Component : Library
 # File      : file.sh
-# Purpose   : File operation helper
+# Purpose   : File helper library
 #
 # SPDX-License-Identifier: MIT
 #
-
-# ==========================
-# Public API
-# ==========================
 
 file_exists() {
     [ -e "$1" ]
@@ -21,19 +17,19 @@ file_read() {
 }
 
 file_write() {
-    printf "%s" "$2" > "$1"
+    printf "%s" "$2" >"$1"
 }
 
 file_append() {
-    printf "%s\n" "$2" >> "$1"
+    printf "%s\n" "$2" >>"$1"
 }
 
 file_copy() {
-    cp "$1" "$2"
+    cp -f "$1" "$2"
 }
 
 file_move() {
-    mv "$1" "$2"
+    mv -f "$1" "$2"
 }
 
 file_remove() {
@@ -41,17 +37,11 @@ file_remove() {
 }
 
 file_backup() {
-    if file_exists "$1"; then
-        cp "$1" "$1.bak"
-    fi
+    file_exists "$1" && cp -f "$1" "$1.bak"
 }
 
 file_restore() {
-    if file_exists "$1.bak"; then
-        mv "$1.bak" "$1"
-    fi
+    file_exists "$1.bak" && mv -f "$1.bak" "$1"
 }
 
-# ==========================
 # End of File
-# ==========================
