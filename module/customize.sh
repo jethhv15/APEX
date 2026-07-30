25cf66e06064bfb7ff3d9862d4d08859e9cb6ddf
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 12:07:39 +0700

    feat(module): add customize script

---
+++ module/customize.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+#
+# APEX
+# customize
+#
+
+ui_print "************************"
+ui_print " Installing APEX"
+ui_print " Android Performance EXperience"
+ui_print "************************"
