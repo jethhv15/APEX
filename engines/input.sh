8fbc33087ada7b1132604dbbc5734c82b427c917
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 14:28:24 +0700

    refactor(engines): migrate all engines to engine lifecycle api

--- engines/input.sh
+++ engines/input.sh
@@ -1,15 +1,23 @@
 #!/system/bin/sh
-#
-# APEX
-# Component : Engine
-# File      : input.sh
-# Purpose   : Input engine
-#
-# SPDX-License-Identifier: MIT
-#
 
-engine_input_apply() {
-    logger_write "ENGINE" "Input engine executed."
+. /data/adb/modules/APEX/lib/engine.sh
+
+ENGINE_NAME="Input"
+
+init() {
+    return 0
 }
 
-# End of File
+apply() {
+    return 0
+}
+
+verify() {
+    return 0
+}
+
+restore() {
+    return 0
+}
+
+run_engine
