d20adb21097063c67f74eed5331cd482ff17aceb
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 12:55:58 +0700

    feat(runtime): implement profile loader

--- core/context.sh
+++ core/context.sh
@@ -10,16 +10,28 @@
 
 CONTEXT_PACKAGE=""
 CONTEXT_PID=""
-CONTEXT_PROFILE=""
+CONTEXT_PROFILE="default"
 
 context_reset() {
     CONTEXT_PACKAGE=""
     CONTEXT_PID=""
-    CONTEXT_PROFILE=""
+    CONTEXT_PROFILE="default"
 }
 
 context_set_package() {
     CONTEXT_PACKAGE="$1"
+
+    case "$1" in
+        com.tencent.ig)
+            CONTEXT_PROFILE="pubg"
+            ;;
+        com.garena.game.codm)
+            CONTEXT_PROFILE="codm"
+            ;;
+        *)
+            CONTEXT_PROFILE="default"
+            ;;
+    esac
 }
 
 context_get_package() {
@@ -32,10 +44,6 @@ context_set_pid() {
 
 context_get_pid() {
     printf "%s" "$CONTEXT_PID"
-}
-
-context_set_profile() {
-    CONTEXT_PROFILE="$1"
 }
 
 context_get_profile() {
