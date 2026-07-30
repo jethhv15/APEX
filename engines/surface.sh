a909c5dd050b21c6a9e2fa72a965cc9fdeccb990
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 14:28:59 +0700

    refactor(engines): migrate all engines to engine lifecycle api

--- engines/surface.sh
+++ engines/surface.sh
@@ -1,15 +1,23 @@
 #!/system/bin/sh
-#
-# APEX
-# Component : Engine
-# File      : surface.sh
-# Purpose   : Surface engine
-#
-# SPDX-License-Identifier: MIT
-#
 
-engine_surface_apply() {
-    logger_write "ENGINE" "Surface engine executed."
+. /data/adb/modules/APEX/lib/engine.sh
+
+ENGINE_NAME="Surface"
+
+init() {
+    return 0
 }
 
-# End of File
+apply() {
+    return 0
+}
+
+verify() {
+    return 0
+}
+
+restore() {
+    return 0
+}
+
+run_engine
