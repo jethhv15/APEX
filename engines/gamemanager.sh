1158a3d4c2d7bf6881079728649eaa8a5c8b0092
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 12:59:37 +0700

    feat(engines): implement GameManager engine

--- engines/gamemanager.sh
+++ engines/gamemanager.sh
@@ -3,13 +3,36 @@
 # APEX
 # Component : Engine
 # File      : gamemanager.sh
-# Purpose   : GameManager engine
+# Purpose   : Android GameManager Engine
 #
 # SPDX-License-Identifier: MIT
 #
 
 engine_gamemanager_apply() {
-    logger_write "ENGINE" "GameManager engine executed."
+    local pkg
+
+    pkg="$(context_get_package)"
+
+    [ -z "$pkg" ] && return 1
+
+    if [ "$(capability_get gamemanager)" != "1" ]; then
+        logger_write "GM" "GameManager not available."
+        return 1
+    fi
+
+    if cmd game >/dev/null 2>&1; then
+        logger_write "GM" "GameManager available for $pkg."
+        #
+        # Future:
+        # cmd game mode performance "$pkg"
+        # cmd game downscale disable "$pkg"
+        #
+        return 0
+    fi
+
+    logger_write "GM" "cmd game unavailable."
+
+    return 1
 }
 
 # End of File
