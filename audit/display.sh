1e09d274380be802cfc74b9dc2ef52e9acafa794
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 13:42:47 +0700

    feat(audit): implement display audit

--- audit/display.sh
+++ audit/display.sh
@@ -8,9 +8,25 @@
 # SPDX-License-Identifier: MIT
 #
 
+DISPLAY_WIDTH="$(wm size 2>/dev/null | awk -F': ' '/Physical size/{print $2}')"
+DISPLAY_DENSITY="$(wm density 2>/dev/null | awk -F': ' '/Physical density/{print $2}')"
+DISPLAY_REFRESH="$(dumpsys display 2>/dev/null | grep -m1 -oE '[0-9]+(\.[0-9]+)?Hz' | tr -d 'Hz')"
+
 audit_display() {
-    registry_set DISPLAY_DENSITY "$(wm density 2>/dev/null | awk '/Physical density/ {print $3}')"
-    registry_set DISPLAY_SIZE "$(wm size 2>/dev/null | awk '/Physical size/ {print $3}')"
+    logger_write "DISPLAY" \
+        "resolution=$DISPLAY_WIDTH density=$DISPLAY_DENSITY refresh=${DISPLAY_REFRESH:-unknown}"
+}
+
+display_get_refresh() {
+    printf "%s" "$DISPLAY_REFRESH"
+}
+
+display_get_resolution() {
+    printf "%s" "$DISPLAY_WIDTH"
+}
+
+display_get_density() {
+    printf "%s" "$DISPLAY_DENSITY"
 }
 
 # End of File
