97df393d564521034276964a42821b958308d8a0
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 19:57:52 +0700

    feat(verify): implement Runtime Verification v2

---
+++ core/runtime_verify.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+#
+# APEX
+# Component : Core
+# File      : runtime_verify.sh
+# Purpose   : Runtime Verification
+#
+# SPDX-License-Identifier: MIT
+#
+
+runtime_verify_run() {
+
+    logger_write "VERIFY" "Runtime verification started."
+
+    if verify_run
+    then
+        logger_write "VERIFY" "Runtime verification passed."
+        return 0
+    fi
+
+    logger_write "VERIFY" "Runtime verification failed."
+
+    restore_run
+
+    return 1
+}
+
+# End of File
