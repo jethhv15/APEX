54dd19472f9d045ba0815e3233c679a2ee2ecf1f
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 14:29:46 +0700

    refactor(engines): migrate all engines to engine lifecycle api

--- engines/gamemanager.sh
+++ engines/gamemanager.sh
@@ -1,38 +1,23 @@
 #!/system/bin/sh
-#
-# APEX
-# Component : Engine
-# File      : gamemanager.sh
-# Purpose   : Android GameManager Engine
-#
-# SPDX-License-Identifier: MIT
-#
 
-engine_gamemanager_apply() {
-    local pkg
+. /data/adb/modules/APEX/lib/engine.sh
 
-    pkg="$(context_get_package)"
+ENGINE_NAME="GameManager"
 
-    [ -z "$pkg" ] && return 1
-
-    if [ "$(capability_get gamemanager)" != "1" ]; then
-        logger_write "GM" "GameManager not available."
-        return 1
-    fi
-
-    if cmd game >/dev/null 2>&1; then
-        logger_write "GM" "GameManager available for $pkg."
-        #
-        # Future:
-        # cmd game mode performance "$pkg"
-        # cmd game downscale disable "$pkg"
-        #
-        return 0
-    fi
-
-    logger_write "GM" "cmd game unavailable."
-
-    return 1
+init() {
+    return 0
 }
 
-# End of File
+apply() {
+    return 0
+}
+
+verify() {
+    return 0
+}
+
+restore() {
+    return 0
+}
+
+run_engine
