03844117c21dd1c9c91bed69e6cbdfb957c107b2
jethhv15 <bapakeeee8@gmail.com>
2026-07-30 12:18:26 +0700

    refactor(lib): improve process helper library

--- lib/process.sh
+++ lib/process.sh
@@ -8,29 +8,24 @@
 # SPDX-License-Identifier: MIT
 #
 
-# Check whether a process exists.
 process_exists() {
     pidof "$1" >/dev/null 2>&1
 }
 
-# Get process PID.
 process_pid() {
     pidof "$1"
 }
 
-# Check whether a process is running.
 process_running() {
     process_exists "$1"
 }
 
-# Kill a process.
 process_kill() {
-    kill "$1"
+    kill "$1" 2>/dev/null
 }
 
-# Kill a process forcefully.
 process_kill_force() {
-    kill -9 "$1"
+    kill -9 "$1" 2>/dev/null
 }
 
 # End of File
